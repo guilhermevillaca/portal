2864ac3865f8ab282da3c40faba6ee6e8df57c5a
Guilherme Luciano Donin Villaca <guilherme.villaca@docente.pr.senac.br>
2024-11-07 18:34:52 -0300

    criando front

--- frontend/portal_front/src/app/app.routes.ts
+++ frontend/portal_front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { NoticiaComponent } from './noticia/noticia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '', component: HomeComponent},
+    {path: 'categoria', component: CategoriaComponent},
+    {path: 'noticia', component: NoticiaComponent}
+];
