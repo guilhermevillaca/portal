7f8cd62ba44f89998bdcd16589d6321fc320a1a5
Guilherme Luciano Donin Villaca <guilherme.villaca@docente.pr.senac.br>
2025-05-28 19:40:21 -0300

    adicionado retorno para usuarioRepository

--- backend/portal_docker/src/main/java/br/com/villaca/portal/repository/UsuarioRepository.java
+++ backend/portal_docker/src/main/java/br/com/villaca/portal/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.villaca.portal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.villaca.portal.modelo.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+    Optional<Usuario> findByLoginAndSenha(String login, String senha);
 
 }
