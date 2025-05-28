d5df9c92bc0283eba9a52ac0523c575ac16c2c73
Guilherme Luciano Donin Villaca <guilherme.villaca@docente.pr.senac.br>
2025-05-28 19:39:30 -0300

    add Login Controller

---
+++ backend/portal_docker/src/main/java/br/com/villaca/portal/controller/LoginController.java
@@ -0,0 +1,35 @@
+package br.com.villaca.portal.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.villaca.portal.dto.LoginDTO;
+import br.com.villaca.portal.modelo.Usuario;
+import br.com.villaca.portal.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findByLoginAndSenha(
+                loginDTO.getLogin(),
+                loginDTO.getSenha());
+
+        if (usuarioOpt.isPresent()) {
+            return ResponseEntity.ok("Login permitido");
+        } else {
+            return ResponseEntity.status(401).body("Login ou senha inválidos");
+        }
+    }
+}
---
+++ backend/portal_docker/src/main/java/br/com/villaca/portal/dto/LoginDTO.java
@@ -0,0 +1,9 @@
+package br.com.villaca.portal.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String login;
+    private String senha;
+}
